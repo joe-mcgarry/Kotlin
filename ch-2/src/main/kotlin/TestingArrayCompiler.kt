f4a74d841c8ae3f27830c0de636469cb1335ac3c
Joe McGarry <joewmcgarry@gmail.com>
2022-08-22 08:31:22 +0100

    Adds chapter 2 work

---
+++ ch-2/src/main/kotlin/TestingArrayCompiler.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+//    val hobbits = arrayOf("Frodo", "Sam", "Merry", "Pippin")
+//    var x = 0
+//
+//    while (x < 5) {
+//        println("${hobbits[x]} is a good Hobbit name")
+//        x += 1
+//    }
+
+    val firemen = arrayOf("Pugh", "Pugh", "Barney", "Cuthbert", "Dibble", "Grub")
+    var firemanNo = 0
+    while (firemanNo < 6) {
+        println("Fireman number $firemanNo is ${firemen[firemanNo]}")
+        firemanNo += 1
+    }
+}
+
