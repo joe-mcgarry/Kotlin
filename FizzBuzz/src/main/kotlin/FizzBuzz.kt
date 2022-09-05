c18f1c189731b7bc11d121542dc623bffd172992
Joe McGarry <joewmcgarry@gmail.com>
2022-09-05 11:37:59 +0100

    Adds tests for fizz and buzz cases

--- FizzBuzz/src/main/kotlin/FizzBuzz.kt
+++ FizzBuzz/src/main/kotlin/FizzBuzz.kt
@@ -3,5 +3,14 @@ fun main(args: Array<String>) {
 }
 
 fun fizzBuzz(num: Int) : String {
-    return num.toString()
+    return if (num % 3 == 0) {
+        "Fizz"
+    }
+    else if (num % 5 == 0) {
+        return "Buzz"
+    }
+    else {
+        return num.toString()
+    }
+
 }
--- FizzBuzz/src/test/kotlin/fizzBuzzTest.kt
+++ FizzBuzz/src/test/kotlin/fizzBuzzTest.kt
@@ -5,6 +5,7 @@ class FizzBuzzTest {
     @Test
     fun testFizzBuzz() {
         assertEquals("1", fizzBuzz(1))
-        assertEquals("1\n 2", fizzBuzz(2))
+        assertEquals("Fizz", fizzBuzz(3))
+        assertEquals("Buzz", fizzBuzz(5))
     }
 }
