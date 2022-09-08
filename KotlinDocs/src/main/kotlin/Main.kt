8f9479cae53db5280496586dfe588b6450b6d9c4
Joe McGarry <joewmcgarry@gmail.com>
2022-09-08 13:54:10 +0100

    Adds directory and inital exercise from Kotlin documentation

---
+++ KotlinDocs/src/main/kotlin/Main.kt
@@ -0,0 +1,33 @@
+fun printMessage(message: String): Unit {
+    println(message)
+}
+
+fun printMessageWithPrefix(message: String, prefix: String = "Info") {
+    println("[$prefix] $message")
+}
+
+fun sum(x: Int, y: Int): Int {
+    return x + y
+}
+
+fun multiply(x: Int, y: Int) = x * y
+
+fun main(args: Array<String>) {
+    infix fun Int.times(str: String) = str.repeat(this)
+    println(2 times "Bye")
+
+    val pair = "Ferrari" to "Katrina"
+    println(pair)
+
+    infix fun String.onto(other: String) = Pair(this, other)
+    val myPair = "McLaren" onto "Lucas"
+
+    val sophia = Person("Sophia")
+    val claudia = Person("Claudia")
+    sophia likes claudia
+}
+
+class Person(val name: String) {
+    val likedPeople = mutableListOf<Person>()
+    infix fun likes(other: Person) { likedPeople.add(other) }
+}
